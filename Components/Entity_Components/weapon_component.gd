81a25df9bf8f97d559feba7e976a5c7090540dc4
Micael <fransisco7117@gmail.com>
2026-03-25 16:45:56 +0100

    WeaponComponent now connects to the AttackComponent of the entity holding the weapon. The weapon that has a weapon component now gains energy after each attack and then triggers the weapon_skill function after sufficient energy is gained

--- Components/Entity_Components/weapon_component.gd
+++ Components/Entity_Components/weapon_component.gd
@@ -1,8 +1,35 @@
 extends Node2D
 class_name WeaponComponent
 
-var max_weapon_energy: int
-var current_energy: int
-var energy_gained_on_attack: int
+var max_weapon_energy: int = 100
+var current_weapon_energy: int = 70
+var weapon_energy_gained_on_attack: int = 10
 
-@onready var parent_entity: Entity = get_parent().get_parent()
+var added_attack_damage_multiplier: int
+
+# Character -> Components -> WeaponSlotComponent -> Weapon -> Components -> WeaponComponent
+@onready var entity_holding_weapon: Entity = get_parent().get_parent().get_parent().get_parent().get_parent()
+
+func _ready() -> void:
+	# This might not be needed when I start spawning in nodes from config files and database
+	await get_tree().process_frame
+	
+	entity_holding_weapon.attack_component.pre_attack_target.connect(_prep_attack)
+	entity_holding_weapon.attack_component.post_attack_target.connect(_finish_attack)
+
+func _prep_attack():
+	entity_holding_weapon.attack_component.weapon_added_multiplier += added_attack_damage_multiplier
+
+func _finish_attack():
+	adjust_energy()
+	entity_holding_weapon.attack_component.weapon_added_multiplier -= added_attack_damage_multiplier
+
+func adjust_energy():
+	current_weapon_energy += weapon_energy_gained_on_attack
+	print("Weapon energy at: " + str(current_weapon_energy) + "/" + str(max_weapon_energy))
+	if current_weapon_energy >= max_weapon_energy:
+		_weapon_skill()
+		current_weapon_energy = 0
+
+func _weapon_skill():
+	print("Using [weapon_skill]")
