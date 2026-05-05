7bf706e1b6a5e7f36b55abfb95fdc5a759cc03bf
Micael <fransisco7117@gmail.com>
2026-05-05 13:44:26 +0200

    Small bug. Clicking play will enter with default team formation.

--- Scenes/Clients/Buttons/start_button.gd
+++ Scenes/Clients/Buttons/start_button.gd
@@ -8,12 +8,14 @@ func _ready():
 	pressed.connect(_button_pressed)
 
 func _button_pressed():
-	var loop_itteration:int = 0
-	for unit in PlayerData.dungeon_team:
-		unit.starting_position = Vector2(50.0 + (100*loop_itteration), 750.0)
-		loop_itteration += 1
-	
-	PlayerData.save_dungeon_team_as_formation()
+	print(str(PlayerData.dungeon_team.size()))
+	if PlayerData.dungeon_team.size() > 0:
+		var loop_itteration:int = 0
+		for unit in PlayerData.dungeon_team:
+			unit.starting_position = Vector2(50.0 + (100*loop_itteration), 750.0)
+			loop_itteration += 1
+			
+		PlayerData.save_dungeon_team_as_formation()
 	
 	var team_lineup_menu: TeamLineupMenu = game_scene.instantiate()
 	
