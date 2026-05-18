e056d1dbead7d27fe7978554a37d9905d24ca6cf
Micael <fransisco7117@gmail.com>
2026-05-18 22:10:17 +0200

    Star level is now displayed on the sprite of weapons.

--- Scripts/Entities/entity_container.gd
+++ Scripts/Entities/entity_container.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name EntityContainer
 
+@onready var star_level_label: Label
+
 @onready var entity: Entity:
 	set(value):
 		entity = value
@@ -9,8 +11,16 @@ class_name EntityContainer
 func _set_sprite():
 	var sprite: Sprite2D = get_child(0)
 	if entity != null:
-		sprite.position = entity.get_node("Sprite/Sprite2D").position
-		sprite.texture = entity.get_node("Sprite/Sprite2D").texture
+		sprite.position = entity.get_node("Sprite2D").position
+		sprite.texture = entity.get_node("Sprite2D").texture
+		
+		star_level_label = get_node("Sprite2D/StarLevelLabel")
+		star_level_label.text = ""
+		var wc: WeaponComponent = entity.get_node_or_null("Components/WeaponComponent")
+		if wc != null:
+			for i in range(wc.star_level):
+				star_level_label.text += "★"
+		
 		return
 	
 	sprite.texture = null
