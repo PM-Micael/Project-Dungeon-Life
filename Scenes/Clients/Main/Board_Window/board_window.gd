ba63b73ea02cf05c0baea0c2a588358bbc3f0587
Micael <fransisco7117@gmail.com>
2026-05-25 01:16:06 +0200

    Changing the sizee of board and inventory will adabt the scale and position of the windows that hold them.

--- Scenes/Clients/Main/Board_Window/board_window.gd
+++ Scenes/Clients/Main/Board_Window/board_window.gd
@@ -6,6 +6,20 @@ class_name BoardWindow
 
 var default_size: Vector2 = Vector2(800, 800)
 
+# Key = board scale, Value = Window position
+var layout_config: Dictionary[String, Vector2] = {
+	"1.0": Vector2(1120, 240),
+	"0.9": Vector2(1200, 320),
+	"0.8": Vector2(1280, 400),
+	"0.7": Vector2(1360, 480),
+	"0.6": Vector2(1440, 560),
+	"0.5": Vector2(1520, 640),
+	"0.4": Vector2(1600, 720),
+	"0.3": Vector2(1680, 800),
+	"0.2": Vector2(1760, 880),
+	"0.1": Vector2(1840, 960),
+}
+
 func _ready() -> void:
 	title = "Window Manager"
 	position = Vector2(1119, 240)
@@ -19,3 +33,5 @@ func _ready() -> void:
 
 func _on_board_scale_changed(board_scale: Vector2):
 	size = default_size * board_scale
+	var key: String = "%.1f" % snappedf(board_scale.x, 0.1)
+	position = layout_config[key]
--- Scenes/Clients/Main/Run_Manager/inventory_window.gd
+++ Scenes/Clients/Main/Run_Manager/inventory_window.gd
@@ -5,9 +5,22 @@ class_name InventoryWindow
 
 var default_size: Vector2 = Vector2(800, 800)
 
+var layout_config: Dictionary[String, Vector2] = {
+	"1.0": Vector2(310, 240),
+	"0.9": Vector2(390, 320),
+	"0.8": Vector2(470, 400),
+	"0.7": Vector2(550, 480),
+	"0.6": Vector2(630, 560),
+	"0.5": Vector2(710, 640),
+	"0.4": Vector2(790, 720),
+	"0.3": Vector2(870, 800),
+	"0.2": Vector2(950, 880),
+	"0.1": Vector2(1030, 960),
+}
+
 func _ready() -> void:
 	title = "Window Manager"
-	position = Vector2(311, 240)
+	position = Vector2(310, 240)
 	unresizable = true
 	#borderless = true
 	always_on_top = true
@@ -18,3 +31,9 @@ func _ready() -> void:
 
 func _on_inventory_scale_changed(inventory_scale: Vector2):
 	size = default_size * inventory_scale
+	var key: String = "%.1f" % snappedf(inventory_scale.x, 0.1)
+	position = layout_config[key]
+
+func _on_board_scale_changed(board_scale: Vector2):
+	var key: String = "%.1f" % snappedf(board_scale.x, 0.1)
+	position.x = layout_config[key].x
