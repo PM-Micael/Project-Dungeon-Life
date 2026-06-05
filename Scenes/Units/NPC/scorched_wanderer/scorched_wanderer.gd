bffc1689fa4d6cd6e19a4d74b0421a0e01dfc48a
Micael <fransisco7117@gmail.com>
2026-06-05 14:04:18 +0200

    Added Scorched wanderer.

---
+++ Scenes/Units/NPC/scorched_wanderer/scorched_wanderer.gd
@@ -0,0 +1,20 @@
+extends Unit
+
+func _init() -> void:
+	id = "scorched_wanderer"
+	
+	base_health = 200
+	attack_damage = 10
+	attack_range = 100
+	base_critical_percent_chance = 0
+	base_critical_damage_multiplier = 1.2
+
+func _ready() -> void:
+	super._ready()
+	_set_stats()
+	_info("scorched_wanderer", "Scorched Wanderer", "Team 2", "Team 1")
+	essence_value = [1, PlayerData.dungeon_layer_level]
+
+func _set_stats():
+	health_component.set_stats(get_total_health())
+	attack_component.set_stats_absolute(get_total_attack_damage(), 100, base_critical_percent_chance, base_critical_damage_multiplier)
