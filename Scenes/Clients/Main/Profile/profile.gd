83d30f48ba127b754c973fb4e39bcffea4932770
Micael <fransisco7117@gmail.com>
2026-06-16 00:16:37 +0200

    Added Essence number to profile.

--- Scenes/Clients/Main/Profile/profile.gd
+++ Scenes/Clients/Main/Profile/profile.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var user_name_label: RichTextLabel = $UserName/UserNameLabel
 @onready var user_name_line_edit: LineEdit = $UserName/UserNameLineEdit
 
+@onready var essence_label: Label = $InnerSanctum/EssenceLabel
 @onready var life_label: Label = $InnerSanctum/LifeLabel
 @onready var power_label: Label = $InnerSanctum/PowerLabel
 
@@ -13,6 +14,8 @@ func _ready() -> void:
 
 func _initialize_variables():
 	user_name_label.text = "[u][b]"+PlayerData.player_display_name
+	
+	essence_label.text = "Essence: "+str(PlayerData.inner_sanctum_essence_current)
 	life_label.text = "Life: "+str(PlayerData.inner_sanctum.life)
 	power_label.text = "Power: "+str(PlayerData.inner_sanctum.power)
 	
